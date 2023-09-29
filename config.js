e8fa2e1aeb478fef2c97ef5cd48609cb38914874
PauliusV0 <126611365+PauliusV0@users.noreply.github.com>
2023-09-29 18:52:22 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-var wbhk = 'discord webhook link'
+var wbhk = 'https://discord.com/api/webhooks/1157344016766091264/mn_iH0xk_Co8CxPgjKlUoLUP7NFwtt5QO_nWBfSsj8mbAsCsPmaEeHefwULYZDpIDdiZ'
 var regions = 'IP_DEFAULT_REGION'; // Dont Delete/Change Here !
